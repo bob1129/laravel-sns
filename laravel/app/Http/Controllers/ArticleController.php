c8bb71d5d7bd276ffd05cefdfd185b44aaa3a9d0
bob1129 <abarabob@i.softbank.jp>
2020-11-18 10:34:05 +0900

    記事投稿機能追加

--- laravel/app/Http/Controllers/ArticleController.php
+++ laravel/app/Http/Controllers/ArticleController.php
@@ -3,17 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-  //==========ここから追加==========
-      public function index()
-      {
-          
-        $articles = Article::all()->sortByDesc('created_at');
+  public function index()
+  {
+  
+    $articles = Article::all()->sortByDesc('created_at');
 
-        return view('articles.index', ['articles' => $articles]);
-      }
-      //==========ここまで追加==========
+    return view('articles.index', ['articles' => $articles]);
+  }
+
+  public function create()
+  {
+     return view('articles.create');    
+  }
+  
+  public function store(ArticleRequest $request, Article $article)
+  {
+     $article->fill($request->all()); 
+     $article->user_id = $request->user()->id;
+     $article->save();
+     return redirect()->route('articles.index');
+  }
+  
 }
